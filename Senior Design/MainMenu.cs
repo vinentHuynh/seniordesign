694c5bf81c99b7bbe50445e6a4b3c5ad2aa87cfe
chuynh017 <christine.huynh017@gmail.com>
2022-05-05 16:54:44 -0500

    added database account verification

--- Senior Design/MainMenu.cs
+++ Senior Design/MainMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data.SqlClient;
 using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
 
 namespace Senior_Design
 {
@@ -17,7 +18,7 @@ namespace Senior_Design
 
             try
             {
-                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                System.Data.SqlClient.SqlConnectionStringBuilder builder = new System.Data.SqlClient.SqlConnectionStringBuilder();
                 builder.DataSource = "sam-database.database.windows.net";
                 builder.UserID = "samadmin";
                 builder.Password = "Seniordesign2022";
@@ -26,7 +27,7 @@ namespace Senior_Design
                 
                 
             }
-            catch (SqlException e)
+            catch (System.Data.SqlClient.SqlException e)
             {
                 Console.WriteLine(e.ToString());
             }
@@ -41,19 +42,34 @@ namespace Senior_Design
         {
             string username;
             string password;
-
+            bool verify = false;
+            
             username = this.txtUsername.Text;
             //need to encrypt password
             password = this.txtPassword.Text;
+
+            string query = "SELECT password FROM users WHERE username = '" + username + "'";
+            ConnectionDB connectionDB = new ConnectionDB();
+            connectionDB.OpenConnection();
+            Microsoft.Data.SqlClient.SqlDataReader dr = connectionDB.DataReader(query);
+            dr.Read();
+            for(int i = 0; i < dr.FieldCount; i++)
+            {
+                string check = dr.GetString(i);
+                if (check == password)
+                    verify = true;
+            }
+            connectionDB.CloseConnection();
+
             //password checking
-            if (username == "admin" && password == "password")
+            if (verify && username == "admin")
             {
                 AdminForm adminform = new AdminForm();
                 adminform.Show();
                 lblError.Hide();
                 this.Hide();
             }
-            else if(username == "user" && password == "password")
+            else if(verify && username != "admin")
             {
                 User user = new User();
                 user.Show();
@@ -73,6 +89,7 @@ namespace Senior_Design
             NewUserForm newuserform = new NewUserForm();
             newuserform.Show(); 
         }
+
 
         private void btnBorrow_Click(object sender, EventArgs e)
         {
