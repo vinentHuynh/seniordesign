380bd27b83cad844dc0cdd78ef2d3fc569aa33e5
Kris <kdzwiener@shockers.wichita.edu>
2022-05-05 19:36:54 -0500

    Test.

---
+++ Senior Design/Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Senior_Design
+{
+    public partial class User : Form
+    {
+        public User()
+        {
+            //initalize
+            InitializeComponent();
+        }
+
+        private void UserClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
