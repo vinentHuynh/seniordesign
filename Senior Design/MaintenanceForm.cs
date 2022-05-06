f645a301c34b5e52b8d0abed8e2a21b935deedb3
chuynh017 <christine.huynh017@gmail.com>
2022-05-05 20:19:21 -0500

    Added asset validation

--- Senior Design/MaintenanceForm.cs
+++ Senior Design/MaintenanceForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using Microsoft.Data.SqlClient;
 
 namespace Senior_Design
 {
@@ -22,23 +23,45 @@ namespace Senior_Design
         {
             int asset_id = int.Parse(txtAssetID.Text);
             string desc = txtDesc.Text;
+            
 
-            string maintIDq = "SELECT COUNT(id) FROM maintenance";
-            ConnectionDB getMaintID = new ConnectionDB();
-            int maintID = getMaintID.ExecuteScalar(maintIDq) + 1;
-            getMaintID.CloseConnection();
+            string checkAssetID = "SELECT id FROM asset";
+            ConnectionDB checkID = new ConnectionDB();
+            checkID.OpenConnection();
+            SqlDataReader dr = checkID.DataReader(checkAssetID);
+            dr.Read();
+            bool verify = false;
+            for(int i = 0; i < dr.FieldCount; i++)
+            {
+                int id = Convert.ToInt32(dr[i].ToString());
+                if (id == asset_id)
+                    verify = true;
+            }
+            if (verify)
+            {
+                string maintIDq = "SELECT COUNT(id) FROM maintenance";
+                ConnectionDB getMaintID = new ConnectionDB();
+                int maintID = getMaintID.ExecuteScalar(maintIDq) + 1;
+                getMaintID.CloseConnection();
 
-            string newMaintReq = "INSERT INTO maintenance(id, asset_id, maintenance_status_id, date_out, date_in, description) " +
-                "VALUES(" + maintID + ", " + asset_id  + ", 2, NULL, NULL, '" + desc + "')";
-            ConnectionDB createMaintReq = new ConnectionDB();
-            createMaintReq.OpenConnection();
-            createMaintReq.ExecuteQueries(newMaintReq);
-            createMaintReq.CloseConnection();
+                string newMaintReq = "INSERT INTO maintenance(id, asset_id, maintenance_status_id, date_out, date_in, description) " +
+                    "VALUES(" + maintID + ", " + asset_id + ", 2, NULL, NULL, '" + desc + "')";
+                ConnectionDB createMaintReq = new ConnectionDB();
+                createMaintReq.OpenConnection();
+                createMaintReq.ExecuteQueries(newMaintReq);
+                createMaintReq.CloseConnection();
 
-            System.Text.StringBuilder sb = new System.Text.StringBuilder();
-            sb.Append("Maintenance request filed. Please contact system administrator for further details.");
-            DialogResult confirmRequest = MessageBox.Show(sb.ToString(), "Request confirmed", MessageBoxButtons.OK);
-            this.Close();
+                System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                sb.Append("Maintenance request filed. Please contact system administrator for further details.");
+                DialogResult confirmRequest = MessageBox.Show(sb.ToString(), "Request confirmed", MessageBoxButtons.OK);
+                this.Close();
+            }
+            else
+            {
+                System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                sb.Append("Invalid Asset Id");
+                DialogResult confirmRequest = MessageBox.Show(sb.ToString(), "Confirm Request", MessageBoxButtons.OK);
+            }
         }
 
         public void btnCancel_Click(object sender, EventArgs e)
