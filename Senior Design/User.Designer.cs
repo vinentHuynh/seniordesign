ded9d0b1ac270a8c40e705471462abe74628df7e
chuynh017 <christine.huynh017@gmail.com>
2022-05-05 20:05:39 -0500

    Finished User view

--- Senior Design/User.Designer.cs
+++ Senior Design/User.Designer.cs
@@ -32,8 +32,15 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.accountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.signOutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tablesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.assetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.maintenanceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -53,10 +60,11 @@
             // 
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.accountToolStripMenuItem});
+            this.accountToolStripMenuItem,
+            this.tablesToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1067, 30);
+            this.menuStrip1.Size = new System.Drawing.Size(1067, 28);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -71,24 +79,87 @@
             // signOutToolStripMenuItem
             // 
             this.signOutToolStripMenuItem.Name = "signOutToolStripMenuItem";
-            this.signOutToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.signOutToolStripMenuItem.Size = new System.Drawing.Size(147, 26);
             this.signOutToolStripMenuItem.Text = "Sign out";
             this.signOutToolStripMenuItem.Click += new System.EventHandler(this.signOutToolStripMenuItem_Click);
+            // 
+            // tablesToolStripMenuItem
+            // 
+            this.tablesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.assetToolStripMenuItem,
+            this.maintenanceToolStripMenuItem});
+            this.tablesToolStripMenuItem.Name = "tablesToolStripMenuItem";
+            this.tablesToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
+            this.tablesToolStripMenuItem.Text = "Tables";
+            // 
+            // assetToolStripMenuItem
+            // 
+            this.assetToolStripMenuItem.Name = "assetToolStripMenuItem";
+            this.assetToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.assetToolStripMenuItem.Text = "Assets";
+            this.assetToolStripMenuItem.Click += new System.EventHandler(this.assetsToolStripMenuItem_Click);
+            // 
+            // maintenanceToolStripMenuItem
+            // 
+            this.maintenanceToolStripMenuItem.Name = "maintenanceToolStripMenuItem";
+            this.maintenanceToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.maintenanceToolStripMenuItem.Text = "Maintenance";
+            this.maintenanceToolStripMenuItem.Click += new System.EventHandler(this.maintenanceToolStripMenuItem_Click);
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(359, 73);
+            this.comboBox1.Location = new System.Drawing.Point(351, 71);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(160, 24);
             this.comboBox1.TabIndex = 2;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(67, 73);
+            this.textBox1.Location = new System.Drawing.Point(82, 73);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(132, 22);
             this.textBox1.TabIndex = 3;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btnFilter_Execute);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Search";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(283, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Column";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(633, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 37);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.txtRefresh_Click);
+            
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(791, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 37);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Search";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnSearch_Click);
             // 
             // User
             // 
@@ -96,6 +167,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.dataGridView1);
@@ -121,5 +196,12 @@
         private System.Windows.Forms.ToolStripMenuItem signOutToolStripMenuItem;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStripMenuItem tablesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem assetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem maintenanceToolStripMenuItem;
     }
 }
--- Senior Design/User.cs
+++ Senior Design/User.cs
@@ -7,17 +7,96 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
 
 namespace Senior_Design
 {
+    
     public partial class User : Form
     {
+        string currentTable = "dbo.asset";
         public User()
         {
             //initalize
             InitializeComponent();
+            showdata();
         }
 
+        void showdata()
+        {
+            ConnectionDB connectionDB = new ConnectionDB();
+            connectionDB.OpenConnection();
+            string query = "SELECT * FROM " + currentTable;
+            bool flag = false;
+            if (currentTable == "dbo.asset")
+            {
+                query += " where deleted = 0";
+                flag = true;
+
+            }
+
+            this.dataGridView1.DataSource = connectionDB.ShowDataInGridView(query);
+            SqlDataReader dr = connectionDB.DataReader(query);
+
+            dr.Read();
+            var columns = new List<string>();
+            columns.Add("");
+            for (int i = 0; i < dr.FieldCount; i++)
+            {
+                columns.Add(dr.GetName(i));
+            }
+            this.comboBox1.DataSource = columns;
+            connectionDB.CloseConnection();
+            if (flag)
+                this.dataGridView1.Columns["deleted"].Visible = false;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (this.comboBox1.Text != "")
+            {
+
+                ConnectionDB connectionDB = new ConnectionDB();
+                connectionDB.OpenConnection();
+                this.dataGridView1.DataSource = connectionDB.ShowDataInGridView("SELECT * from " + currentTable + " WHERE " + this.comboBox1.Text +
+                    " LIKE '%" + this.textBox1.Text + "%'");
+            }
+            else if (this.textBox1.Text == null)
+            {
+                ConnectionDB connectionDB = new ConnectionDB();
+                connectionDB.OpenConnection();
+                this.dataGridView1.DataSource = connectionDB.ShowDataInGridView("SELECT * from " + currentTable + " WHERE " + this.comboBox1.Text +
+                    " LIKE '%" + this.textBox1.Text + "%'");
+            }
+            else
+            {
+                DialogResult res = MessageBox.Show("Please select a column.", "Confirmation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private void maintenanceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            currentTable = "dbo.maintenance";
+            showdata();
+        }
+
+        private void txtRefresh_Click(object sender, EventArgs e)
+        {
+            showdata();
+            this.textBox1.Text = "";
+        }
+        private void assetsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            currentTable = "dbo.asset";
+            showdata();
+        }
+
+        private void btnFilter_Execute(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnSearch_Click(this, new EventArgs());
+            }
+        }
         private void UserClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
@@ -30,7 +109,11 @@ namespace Senior_Design
 
         private void signOutToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            this.Hide();
+            MainMenu mainMenu = new MainMenu();
+            mainMenu.Show();
         }
+
+  
     }
 }
